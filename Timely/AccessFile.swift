9e64e4e068a6889523c5910fda91dfda8087fc92
Parcley27 <95258151+Parcley27@users.noreply.github.com>
2023-11-12 15:16:49 -0800

    Add Names.txt

--- Timely/AccessFile.swift
+++ Timely/AccessFile.swift
@@ -2,17 +2,77 @@
 //  AccessFile.swift
 //  Timely
 //
-//  Created by Pierce Oxley on 2023-11-12.
+//  Created by Pierce Oxley on 2023-07-23.
+//
+//  Does not work, file always not found
 //
 
-import SwiftUI
+import Foundation
 
-struct AccessFile: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+open class contentDetail {
+    
+    var names = ["James", "Austin", "Alex", "Pierce", "Wade", "Guy", "Dale", "Akshay", "Aurian", "Jayden", "Aleksandra", "Dad", "Mom", "Joel", "Jeevan", "Sophie", "Keeley", "Molly", "Zoie", "Matteo", "Luca", "Armaan"]
+    
+    func compileDetailInfomation(dName: String, dDate: String, dValues: String, dMisc: String) -> [String] {
+        let detailInformation = [dName, dDate, dValues, dMisc]
+        return detailInformation
     }
-}
+    
+    //if let filedNames = nameStorage().accessFile(file: "/names", isReading: true, fileLine: 2)
+    /*
+    func accessFile(file: String, isReading: Bool, fileLine: Int, dataToWrite: String? = nil) -> String? {
+        // Get file URL, and throw error if not lost
+        guard let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
 
-#Preview {
-    AccessFile()
+            print("Error. File Address Not Found.")
+        
+            return nil
+        }
+        
+        let fileURL = documentDirectoryURL.appendingPathComponent(file)
+
+        
+        // Read from file
+        if isReading == true {
+            
+            // do{} saves sketchy code from because of catch{} as a safety net
+            do {
+                let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
+                let lines = fileContents.components(separatedBy: .newlines)
+                let totalLines = lines.count
+                
+                var readData: String? = nil
+                
+                if fileLine == 0 {
+                    readData = lines.last
+                    
+                } else if fileLine > 0 && fileLine <= totalLines {
+                    readData = lines[fileLine - 1]
+                    
+                } else {
+                    print("Error. Wanted line (\(fileLine)) is out of available range.")
+                    return nil
+                }
+                
+                // Returns result of found line
+                return readData
+                
+            } catch {
+                print("Error. Problem reading from file with \(error)")
+                return nil
+            }
+            
+        } else {
+            print("Write Requested")
+        }
+        
+        return nil
+    }
+     */
+    
+    func oogabooga() -> String? {
+        let result = "oogabooga is Working"
+        return result
+
+    }
 }
