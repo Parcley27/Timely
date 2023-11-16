fcbf075448fbe7025007074fb78a380c4a78c681
Parcley27 <95258151+Parcley27@users.noreply.github.com>
2023-11-15 18:07:27 -0800

    Work with Swift Data later

--- Timely/Item.swift
+++ Timely/Item.swift
@@ -5,14 +5,19 @@
 //  Created by Pierce Oxley on 2023-11-12.
 //
 
-import Foundation
-import SwiftData
-
-@Model
-final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
-}
+//Error with Swift Data
+/*
+ 
+ import Foundation
+ import SwiftData
+ 
+ @Model
+ final class Item {
+ var timestamp: Date
+ 
+ init(timestamp: Date) {
+ self.timestamp = timestamp
+ }
+ }
+ 
+ */
