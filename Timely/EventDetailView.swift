d9a8e05df3a0f3c4f99bc1eb0ea488ec440feb36
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-01-12 12:05:43 -0800

    feat: show data with options to edit and delete the event

--- Timely/EventDetailView.swift
+++ Timely/EventDetailView.swift
@@ -10,52 +10,97 @@ import SwiftUI
 struct EventDetailView: View {
     @EnvironmentObject var data: EventData
     
-    let event: Event
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State var event: Event
         
     @State private var timeUpdater: String = ""
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var confirmationIsShowing = false
+    
+    private var deleteButton: some View {
+        HStack {
+            Text("Delete")
+            Spacer()
+            Image(systemName: "trash")
+        }
+        .foregroundStyle(.red)
+    }
     
     var body: some View {
         NavigationStack {
             //let navigationTitleWrapper = (event.emoji ?? "📅") + " " + (event.name ?? "Event Name")
             let navigationTitleWrapper = (event.name ?? "Details")
             
-            VStack {
-                Spacer()
-                
-                let titleWrapper = (event.emoji ?? "📅") + " " + (event.name ?? "Event Name")
-                
-                Text(titleWrapper)
-                    .font(.largeTitle)
-                    .bold()
-                
-                Text(event.timeUntil + timeUpdater)
-                    .font(.title)
-                    .onReceive(timer) { _ in
-                        // Reset timeUpdater every second
-                        // This tricks the text object into getting a new timeUntil
-                        timeUpdater = " "
-                        timeUpdater = ""
-                    }
-                    .foregroundStyle(event.timeUntil.hasPrefix("-") == true ? .red : .primary)
-                    .bold(event.timeUntil.hasPrefix("-") == true)
-
-                
-                List {
-                    Section("Date") {
-                        Text(data.dateDisplayString(event: event))
-                        //Text(data.timeUntil(inputDate: event.dateAndTime, format: "Seconds"))
+            List {
+                Section {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(event.name ?? "Event Name")
+                                .font(.title)
+                                .bold()
+                            Text(event.timeUntil + timeUpdater)
+                                .onReceive(timer) { _ in
+                                    timeUpdater = " "
+                                    timeUpdater = ""
+                                }
+                                .foregroundStyle(event.timeUntil.hasPrefix("-") == true ? .red : .primary)
+                                .bold(event.timeUntil.hasPrefix("-") == true)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(event.emoji ?? "📅")
+                            .font(.system(size: 42))
                     }
                 }
-                .listStyle(.inset)
                 
-                /*
-                 Button("Change name") {
-                 let newName = "newName test"
-                 data.updateEventName(event: event, newName: newName)
-                 
-                 }
-                 */
+                Section {
+                    HStack {
+                        Text(event.dateString ?? "Event date and time")
+                            .foregroundStyle(event.timeUntil.hasPrefix("-") == true ? .red : .primary)
+                            .bold(event.timeUntil.hasPrefix("-") == true)
+                    }
+                }
+                
+                if event.description != nil {
+                    Section {
+                        Text(event.description ?? "")
+                    }
+                }
+                
+                Section {
+                    Toggle("Favourite", isOn: $event.isFavourite)
+                        .onChange(of: event.isFavourite) { newValue in
+                            data.toggleFavouriteEvent(event: event)
+                        }
+                    
+                    Toggle("Mute", isOn: $event.isMuted)
+                        .onChange(of: event.isMuted) { newValue in
+                            data.toggleMutedEvent(event: event)
+                        }
+                }
+
+                Section {
+                    Button {
+                        confirmationIsShowing = true
+                    } label: {
+                        deleteButton
+                    }
+                        .actionSheet(isPresented: $confirmationIsShowing) {
+                            ActionSheet(
+                                title: Text("This action can not be undone"),
+                                buttons: [
+                                    .cancel(Text("Cancel")),
+                                    .destructive(Text("Delete Event"), action: {
+                                        data.removeEvent(event:event)
+                                        presentationMode.wrappedValue.dismiss()
+                                    })
+                                ]
+                            )
+                        }
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
