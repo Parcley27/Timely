d4ea326a73e2923278d17df5f3828a42087bceb7
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-01-26 23:33:21 -0800

    Create EditEventSheetView.swift

---
+++ Timely/EditEventSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventSheetView.swift
+//  Timely
+//
+//  Created by Pierce Oxley on 2024-01-26.
+//
+
+import SwiftUI
+
+struct EditEventSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEventSheetView()
+}
