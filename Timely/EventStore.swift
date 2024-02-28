4f80639aeafcedc10577f66d4543afbe0b2b751e
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-02-27 21:45:10 -0800

    add EventStore.swift

---
+++ Timely/EventStore.swift
@@ -0,0 +1,46 @@
+//
+//  EventStore.swift
+//  Timely
+//
+//  Created by Pierce Oxley on 2024-02-15.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+class EventStore: ObservableObject {
+    @Published var events: [Event] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, 
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("events.data")
+    }
+    
+    func load() async throws {
+        let task = Task<[Event], Error> {
+            let fileURL = try Self.fileURL()
+            guard let data = try? Data(contentsOf: fileURL) else {
+                return []
+            }
+            
+            let loadedEvents = try JSONDecoder().decode([Event].self, from: data)
+            return loadedEvents
+        }
+        
+        let events = try await task.value
+        self.events = events
+    }
+    
+    func save(events: [Event]) async throws {
+        let task = Task {
+            let data = try JSONEncoder().encode(events)
+            let outfile = try Self.fileURL()
+            try data.write(to: outfile)
+        }
+        _ = try await task.value
+    }
+}
