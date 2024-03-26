ad43336c14c304c407aaca202ee7de6aa81640a9
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-03-25 22:31:41 -0700

    feat: pass event data to CalendarView

--- Timely/CalendarView.swift
+++ Timely/CalendarView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @Binding var data: [Event]
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CalendarView()
+    let previewData = EventData()
+    previewData.events = [
+        Event(name: "Sample Event 1", dateAndTime: Date()),
+        Event(name: "Sample Event 2", isMuted: true),
+        Event(name: "Sample Event 3", isFavourite: true)
+        // Add more sample events if needed
+    ]
+    
+    let previewEvents = Binding.constant(previewData.events)
+    
+    return CalendarView(data: previewEvents)
 }
