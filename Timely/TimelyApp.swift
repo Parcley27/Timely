951aee9d43b7f364982cc220ca91496077d7714b
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-03-26 20:46:42 -0700

    feat: add events from center button

--- Timely/TimelyApp.swift
+++ Timely/TimelyApp.swift
@@ -5,12 +5,6 @@
 //  Created by Pierce Oxley on 2023-06-14.
 //
 
-// Change color with system theme
-//
-//@Environment(\.colorScheme) var colorScheme
-//
-//.foregroundColor(colorScheme == .dark ? .white : .black)
-
 import SwiftUI
 
 @main
@@ -22,42 +16,133 @@ struct TimelyApp: App {
         
         return passedEvents
     }
+    
+    @State var selectedTab: Int = 0
+    @State var lastTab: Int = 0
+
+    @State private var showNewSheet: Bool = false
+    
+    var plusButton: some View {
+        ZStack {
+            Circle()
+                .stroke(.tertiary, lineWidth: 5)
+                .frame(width: 100)
+                .background(
+                      .bar,
+                      in: Circle()
+                   )
+
+            Image(systemName: "plus")
+                .resizable()
+                .frame(width: 50.0, height: 50.0)
+                .foregroundStyle(.secondary)
+            
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
-            TabView {
-                EventListView(data: $eventList.events) {
-                    Task {
-                        do {
-                            try await eventList.save(events: eventList.events)
-                        } catch {
-                            fatalError(error.localizedDescription)
+            VStack {
+                ZStack {
+                    TabView(selection: $selectedTab) {
+                        Group {
+                            EventListView(data: $eventList.events) {
+                                Task {
+                                    do {
+                                        try await eventList.save(events: eventList.events)
+                                    } catch {
+                                        fatalError(error.localizedDescription)
+                                    }
+                                }
+                            }
+                            .task {
+                                do {
+                                    try await eventList.load()
+                                    print("Loading events: \(eventList.events)")
+                                } catch {
+                                    fatalError(error.localizedDescription)
+                                }
+                            }
+                            .badge(filterPassedEvents(events: eventList.events).count)
+                            .tabItem {
+                                Label("Events", systemImage: "list.bullet")
+                            }
+                            .tag(0)
+                            
+                            if lastTab == 0 {
+                                EventListView(data: $eventList.events) {
+                                    Task {
+                                        do {
+                                            try await eventList.save(events: eventList.events)
+                                        } catch {
+                                            fatalError(error.localizedDescription)
+                                        }
+                                    }
+                                }
+                                .tag(1)
+                            } else if lastTab == 2 {
+                                CalendarView(data: $eventList.events) {
+                                    Task {
+                                        do {
+                                            try await eventList.save(events: eventList.events)
+                                        } catch {
+                                            fatalError(error.localizedDescription)
+                                        }
+                                    }
+                                }
+                                .tag(1)
+                            }
+                            
+                            CalendarView(data: $eventList.events) {
+                                Task {
+                                    do {
+                                        try await eventList.save(events: eventList.events)
+                                    } catch {
+                                        fatalError(error.localizedDescription)
+                                    }
+                                }
+                            }
+                            .task {
+                                do {
+                                    try await eventList.load()
+                                    print("Loading events: \(eventList.events)")
+                                } catch {
+                                    fatalError(error.localizedDescription)
+                                }
+                            }
+                            .tabItem {
+                                Label("Calendar", systemImage: "calendar")
+                            }
+                            .tag(2)
                         }
+                    }
+                    .onChange(of: selectedTab) { newTab in
+                        if newTab == 0 || newTab == 2 {
+                            lastTab = selectedTab
+                            print(selectedTab)
+                        }
+                        
+                        if newTab == 1 {
+                            print(lastTab)
+                            selectedTab = lastTab
+                            print("Switching tab")
+                            print(selectedTab)
+                        }
+                    }
+                    
+                    GeometryReader { metrics in
+                        plusButton
+                            .frame(width: metrics.size.width / 4.5, height: metrics.size.height / 9.0)
+                            .onTapGesture {
+                                print("plus")
+                                showNewSheet = true
+                            }
+                            .position(x: metrics.size.width * 0.5, y: metrics.size.height - 48)
+                    }
+                    .sheet(isPresented: $showNewSheet) {
+                        NewEventSheetView(data: $eventList.events)
                     }
                 }
-                .badge(filterPassedEvents(events: eventList.events).count)
-                    .tabItem {
-                        Label("Events", systemImage: "list.bullet")
-                    }
-                
-                    .task {
-                        do {
-                            try await eventList.load()
-                            print("Loading events: \(eventList.events)")
-                        } catch {
-                            fatalError(error.localizedDescription)
-                        }
-                    }
-                
-                //Spacer()
-                // Use spacer to give room for large add button later
-                
-                //Settings(data: $eventList.events)
-                CalendarView(data: $eventList.events)
-                    //.badge()
-                    .tabItem {
-                        Label("Settings", systemImage: "gearshape")
-                    }
             }
         }
     }
