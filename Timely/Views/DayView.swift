44ca73ad684e49c815d50ef1a959e32964688fc0
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-04-19 22:13:36 -0700

    Create DayView.swift

---
+++ Timely/Views/DayView.swift
@@ -0,0 +1,18 @@
+//
+//  DayView.swift
+//  Timely
+//
+//  Created by Pierce Oxley on 2024-04-19.
+//
+
+import SwiftUI
+
+struct DayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayView()
+}
